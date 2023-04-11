791860a8804710200d77dc17a4a58749dc2b23c6
Sam <samdc@apadmi.com>
2023-04-11 19:59:42 +0100

    A

--- Package.swift
+++ Package.swift
@@ -19,7 +19,8 @@ let package = Package(
             path: "./LPMessagingSDKModels.bundle",
             resources: [
                 .process("./**")
-            ]
+            ],
+            defaultLocalization: "en"
         ),
         .binaryTarget(
             name: "LPMessagingSDK",
