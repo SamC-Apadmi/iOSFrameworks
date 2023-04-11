027415afde2ea345e02f5a83c662819d2050b18e
Sam <samdc@apadmi.com>
2023-04-11 19:51:16 +0100

    and again

--- Package.swift
+++ Package.swift
@@ -6,23 +6,20 @@ import PackageDescription
 let package = Package(
     name: "LPMessagingSDK",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v)
     ],
     products: [
         .library(
             name: "LPMessagingSDK",
-            targets: ["LPMessagingSDK", "LPMessagingSDKResources"])
+            targets: ["LPMessagingSDK"])
     ],
     targets: [
         .binaryTarget(
-            name: "LPMessagingSDKResources",
-            resources: [
-                .process("LPMessagingSDKModels.bundle")
-            ]
-        ),
-        .binaryTarget(
             name: "LPMessagingSDK",
-            path: "LPMessagingSDK.xcframework"
+            path: "LPMessagingSDK.xcframework",
+            resources: [
+                .process("LPMessagingSDKModels.bundle/**")
+            ]
         )
     ],
     swiftLanguageVersions: [.v5]
