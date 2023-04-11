31d976ec0b993f39372a1b86435c2f8302ecfac7
Sam <samdc@apadmi.com>
2023-04-11 19:57:16 +0100

    A

--- Package.swift
+++ Package.swift
@@ -16,6 +16,7 @@ let package = Package(
     targets: [
         .target(
             name: "LPMessagingSDKResources",
+            path: "./LPMessagingSDKModels.bundle",
             resources: [
                 .process("./**")
             ]
